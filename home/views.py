267fc699005bbeb7086538347d482ce345291a88
unknown <mishrakitirajeshkumar@gmail.com>
2022-11-25 16:04:16 +0530

    my first commit

---
+++ home/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    
+    return render(request,"home.html",)
+
+def result(request):
+    article=request.GET['article']
+    words=article.split()
+    word_count=len(words)
+    
+    dict_words={}
+    for word in words: 
+        if word in dict_words:
+            dict_words[word] += 1
+        else:
+            dict_words[word] = 1
+            
+    return render(request,"result.html",{"article":article,'word_count':word_count,'dict_words':dict_words})
